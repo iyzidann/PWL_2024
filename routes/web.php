8027bb3736302f57333db9ced193975fdd73f087
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2024-09-04 09:29:17 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic Routing
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return '2241760128 / Muhammad Iyazuz Zidan';
+}); 
+
+// Route Parameters
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya '.$name;
+}); 
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+// Optional Parameters
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+// Route Name
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
